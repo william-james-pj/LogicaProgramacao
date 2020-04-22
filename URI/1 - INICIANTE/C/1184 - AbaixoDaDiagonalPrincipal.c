6c553dac423037e13e194b7291245b8c6fdc74da
William James <52788612+william-james-pj@users.noreply.github.com>
2020-04-22 11:40:12 -0300

    Abaixo da Diagonal Principal

---
+++ URI/1 - INICIANTE/C/1184 - AbaixoDaDiagonalPrincipal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	char op;
+	double matriz[12][12], soma;
+	int linhaCont, colunaCont, x,cont = 0;
+	
+	scanf(" %c", &op);
+	
+	for(linhaCont = 0; linhaCont < 12; linhaCont++){
+		for(colunaCont = 0; colunaCont < 12; colunaCont++){
+			scanf(" %lf", &matriz[linhaCont][colunaCont]);
+		}
+	}
+	for(linhaCont = 1; linhaCont < 12; linhaCont++){
+		x += 1;
+		for(colunaCont = 0; colunaCont < x; colunaCont++){
+			soma += matriz[linhaCont][colunaCont];
+			cont += 1;
+		}
+	}
+	if(op == 'S'){
+		printf("%.1lf\n", soma);
+	}else{
+		printf("%.1lf\n", soma/cont);
+	}
+	
+	return 0;
+}
