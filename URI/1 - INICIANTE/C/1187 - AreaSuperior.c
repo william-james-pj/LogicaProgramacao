477f344385db702b2f06794067d964646e99f9e8
William James <52788612+william-james-pj@users.noreply.github.com>
2020-04-22 11:41:15 -0300

    Area Superior

---
+++ URI/1 - INICIANTE/C/1187 - AreaSuperior.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	char op;
+	double matriz[12][12], soma;
+	int linhaCont, colunaCont, posicaoInicial = 0, posicaoFinal = 12,cont = 0;
+	scanf(" %c", &op);
+	
+	for(linhaCont = 0; linhaCont < 12; linhaCont++){
+		for(colunaCont = 0; colunaCont < 12; colunaCont++){
+			scanf(" %lf", &matriz[linhaCont][colunaCont]);
+		}
+	}
+	for(linhaCont = 0; linhaCont < 5; linhaCont++){
+		posicaoInicial += 1;
+		posicaoFinal -= 1;
+		for(colunaCont = posicaoInicial; colunaCont < posicaoFinal; colunaCont++){
+			soma += matriz[linhaCont][colunaCont];
+			cont += 1;
+		}
+	}
+	if(op == 'S'){
+		printf("%.1lf\n", soma);
+	}else{
+		printf("%.1lf\n", soma/cont);
+	}
+	
+	return 0;
+}
---
+++ URI/1 - INICIANTE/Python/1187 - AreaSuperior.py
@@ -0,0 +1,21 @@
+op = input()
+matriz = []
+soma = 0.0
+posicaoInicial = 0
+posicaoFim = 12
+total = 0
+for x in range(0, 12):
+    linha = []
+    for y in range(0, 12):
+        linha.append(float(input()))
+    matriz.append(linha)
+for linhaCont in range(0, 5):
+    posicaoInicial += 1
+    posicaoFim -= 1
+    for colunaCont in range(posicaoInicial, posicaoFim):
+        soma += matriz[linhaCont][colunaCont]
+        total += 1
+if op == 'S':
+    print('{:.1f}'.format(soma))
+else:
+    print('{:.1f}'.format(soma/total))
