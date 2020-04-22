27faa042f2dcd954f8fd629d5663a43faf3c2a01
William James <52788612+william-james-pj@users.noreply.github.com>
2020-04-22 11:45:10 -0300

    Vitoria e a Indecisao

---
+++ URI/1 - INICIANTE/C/1924 - VitoriaEAIndecisao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int z;
+	char string[100];
+	scanf(" %d", &z);
+	while(z > 0){
+		scanf(" %s", string);
+		z--;
+	}
+	printf("Ciencia da Computacao\n");
+	return 0;
+}
---
+++ URI/1 - INICIANTE/Python/1924 - VitoriaEAIndecisao.py
@@ -0,0 +1,5 @@
+z = int(input())
+while z > 0:
+    a = input()
+    z -= 1
+print('Ciencia da Computacao')
